01b5193d03d711baa126a66e516afc53e555f04a
whatasoda <git@whatasoda.me>
2019-11-22 21:15:15 +0900

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Scheduler from './scheduler';
+import defineNode from './node';
+import Vector from './vector';
+
+const vn = { Scheduler, defineNode, Vector };
+
+export * from 'decls';
+export { Scheduler, defineNode, Vector };
+export default vn;
